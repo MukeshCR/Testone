08e19ac7d6da29f1a2c748c6db5070bc806a0f16
Mukesh Chakali Radhakrishna <mukesh.radhakrishna@qapitol.com>
2024-08-20 11:55:26 +0530

    added code for adding product to cart

--- src/test/java/Launch/eCommerceTest02.java
+++ src/test/java/Launch/eCommerceTest02.java
@@ -3,8 +3,14 @@ package Launch;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class eCommerceTest02 extends BaseTest{
     @Test
@@ -17,6 +23,9 @@ public class eCommerceTest02 extends BaseTest{
         driver.findElement(
                 AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+        // selecting one of the product by scrolling to it and verifiying the product by text and adding it to the cart
+
         driver.findElement(
                 AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Air Jordan 9 Retro\"));"));
         int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
@@ -28,6 +37,12 @@ public class eCommerceTest02 extends BaseTest{
         }
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+
+        String lastpageproductName = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+        Assert.assertEquals(lastpageproductName, "Air Jordan 9 Retro");
 
     }
 }
+
